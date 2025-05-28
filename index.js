36d719f7af2654f3cae49e01629da74fd0a528fd
Dav <david.sanchez.martinez@outlook.com>
2025-05-28 20:01:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ scoreBtns.forEach((button) => {
     button.addEventListener("click", function() {
         const team = button.dataset.team
         const score = parseInt(button.dataset.score)
-        // const scoreDiv = document.getElementById(`${team}-score`)
-        // const span = scoreDiv.querySelector("span")
         const teamScore = document.getElementById(`${team}-score`)
 
         let intScore = parseInt(teamScore.textContent)
